89b8032755686757deb6cafa11eee53c3b7f30c2
efallash <efallashdez@gmail.com>
2024-07-03 11:26:05 +0200

    Implemented publishers and subscribers for activation

--- core/core/cognitive_node.py
+++ core/core/cognitive_node.py
@@ -1,8 +1,10 @@
 import yaml
 import inspect
+import numpy
 from rclpy.node import Node
 from rclpy import spin_until_future_complete
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.time import Time
 
 from core.service_client import ServiceClient, ServiceClientAsync
 from core_interfaces.srv import AddNodeToLTM, DeleteNodeFromLTM
@@ -38,9 +40,13 @@ class CognitiveNode(Node):
 
         # self.threshold = threshold
         self.neighbors = [] # List of dics, like [{"name": "pnode1", "node_type": "PNode"}, {"name": "cnode1", "node_type": "CNode"}]
-
-        self.activation_topic = False
-        self.activation = 0.0
+        
+        self.activation_inputs={}
+        self.activation_sources=[] #List of node types that provide activations for a node, must be populated in each node type
+        self.activation_topic = True
+        self.activation = Activation()
+        self.activation.node_name=self.name
+        self.activation.node_type=self.node_type
 
         self.perception = []
         self.threshold = 0.0
@@ -50,6 +56,7 @@ class CognitiveNode(Node):
 
         self.cbgroup_server=MutuallyExclusiveCallbackGroup()
         self.cbgroup_client=MutuallyExclusiveCallbackGroup()
+        self.cbgroup_activation=MutuallyExclusiveCallbackGroup()
 
         self.node_clients={} #Keys are service name, values are service client object e.g. {'cognitive_node/policy0/get_activation: "Object: Node.client"'}
 
@@ -95,6 +102,8 @@ class CognitiveNode(Node):
             self.delete_neighbor_callback, callback_group=self.cbgroup_server
         )
 
+        self.activation_publish_timer=self.create_timer(0.1, self.publish_activation_callback, callback_group=self.cbgroup_activation)
+
         service_name_add_LTM = 'ltm_0' + '/add_node' # TODO choose LTM ID
         self.add_node_to_LTM_client = ServiceClientAsync(self, AddNodeToLTM, service_name_add_LTM, self.cbgroup_client)
 
@@ -139,7 +148,7 @@ class CognitiveNode(Node):
         """
         self.get_logger().debug(f'DEBUG START Registering {self.node_type} {self.name} in LTM...')
         
-        data = yaml.dump({**data_dic, 'activation': self.activation, 'perception': self.perception, 'neighbors': self.neighbors})
+        data = yaml.dump({**data_dic, 'activation': self.activation.activation, 'perception': self.perception, 'neighbors': self.neighbors})
 
         ltm_response = self.add_node_to_LTM_client.send_request_async(name=self.name, node_type=self.node_type, data=data)
         await ltm_response
@@ -156,27 +165,45 @@ class CognitiveNode(Node):
         ltm_response = self.delete_node_client.send_request_async(name=self.name)
         return ltm_response.deleted
    
-    def calculate_activation(self, perception):
+    def calculate_activation(self, perception, activation_list):
         """
         Calculate the node's activation for the given perception.
         :param perception: The perception for which the activation will be calculated.
         :type perception: float
+        :param activation_list: List of activations considered in the node
+        :type activation_list: dict
         """
         raise NotImplementedError
-    
-    def publish_activation(self, activation):
-        """
-        Publish the activation of this node.
-        :param activation: The activation to be published.
-        :type activation: float
-        """
-        msg = Activation()
-        msg.activation = activation
-        msg.node_name = self.name
-        msg.node_type = self.node_type
-        self.publish_activation_topic.publish(msg)
-        self.get_logger().info("Activation for " + str(msg.node_type) + str(msg.node_name) +
-                               ": " + str(msg.activation))
+
+    def calculate_activation_prod(self, activation_list):
+        node_activations = [activation_list[node_name]['data'].activation for node_name in activation_list]
+        if len(node_activations)!=0:
+            activation=numpy.prod(node_activations)
+        else:
+            self.get_logger().debug(f'{self.name} -- Node activation list empty!!')
+            activation=0.0
+        self.activation.activation=float(activation)
+        self.activation.timestamp=self.get_clock().now().to_msg()
+
+    def calculate_activation_max(self, activation_list):
+        node_activations = [activation_list[node_name]['data'].activation for node_name in activation_list]
+        if len(node_activations)!=0:
+            activation=numpy.max(node_activations)
+        else:
+            self.get_logger().debug(f'{self.name} -- Node activation list empty!!')
+            activation=0
+        self.activation.activation=float(activation)
+        self.activation.timestamp=self.get_clock().now().to_msg()
+
+    def publish_activation(self, activation: Activation):
+            """
+            Publish the activation of this node.
+            :param activation: The activation to be published.
+            :type activation: float
+            """
+            self.publish_activation_topic.publish(activation)
+            self.get_logger().debug("Activation for " + str(activation.node_type) + str(activation.node_name) +
+                                ": " + str(activation.activation))
         
     def add_neighbor_callback(self, request, response):
         """
@@ -195,6 +222,7 @@ class CognitiveNode(Node):
         self.get_logger().debug(f'Adding {type} {name} as neighbor of {self.node_type} {self.name}')
         neighbor = {'name':name, 'node_type':type}
         self.neighbors.append(neighbor)
+        self.create_activation_input(neighbor)
         response.added = True
         return response
     
@@ -216,6 +244,7 @@ class CognitiveNode(Node):
         for neighbor in self.neighbors:
             if neighbor == neighbor_to_delete:
                 self.neighbors.remove(neighbor)
+                self.delete_activation_input(neighbor_to_delete)
                 response.deleted = True
             else:
                 response.deleted = False
@@ -238,7 +267,7 @@ class CognitiveNode(Node):
             await self.calculate_activation(perception)
         else:
             self.calculate_activation(perception)
-        response.activation = float(self.activation)
+        response.activation = float(self.activation.activation)
         return response
 
     def get_information_callback(self, request, response):
@@ -254,7 +283,7 @@ class CognitiveNode(Node):
         self.get_logger().debug('Getting node information...')
         response.node_name = self.name
         response.node_type = self.node_type
-        response.current_activation = self.activation
+        response.current_activation = self.activation.activation
         response.neighbors_name = [neighbor["name"] for neighbor in self.neighbors]
         response.neighbors_type = [neighbor["node_type"] for neighbor in self.neighbors]
         self.get_logger().debug("The type of the node " + str(response.node_name) + "is " + str(response.node_type) +
@@ -279,6 +308,59 @@ class CognitiveNode(Node):
         self.activation_topic = activation_topic
         response.activation_topic = activation_topic
         return response
+    
+    async def publish_activation_callback(self): #Timed publish of the activation value
+        if self.activation_topic:
+            if len(self.activation_inputs)==0: #Calculates activation when there are no inputs configured (Support for custom nodes)
+                updated=True
+            else:
+                self.get_logger().debug(f'{self.name} -- Activation Inputs: {str(self.activation_inputs)}')
+                updated= all((self.activation_inputs[node_name]['updated'] for node_name in self.activation_inputs)) 
+
+            if updated:
+                if inspect.iscoroutinefunction(self.calculate_activation):
+                    await self.calculate_activation(perception=None, activation_list=self.activation_inputs)
+                else:
+                    self.calculate_activation(perception=None, activation_list=self.activation_inputs)
+                for node_name in self.activation_inputs:
+                    self.activation_inputs[node_name]['updated']=False
+            self.publish_activation(self.activation)
+
+    def create_activation_input(self, node: dict): #Adds a node from the activation inputs list. By default reads activations.
+        name=node['name']
+        node_type=node['node_type']
+        if name not in self.activation_inputs:
+            if node_type in self.activation_sources:
+                subscriber=self.create_subscription(Activation, 'cognitive_node/' + str(name) + '/activation', self.read_activation_callback, 1, callback_group=self.cbgroup_activation)
+                data=Activation()
+                updated=False
+                new_input=dict(subscriber=subscriber, data=data, updated=updated)
+                self.activation_inputs[name]=new_input
+                self.get_logger().info(f'{self.name} -- Created new activation input: {name} of type {node_type}')
+                self.get_logger().info(f'PRUEBA {self.name} -- {str(self.activation_inputs)}')
+            else:
+                self.get_logger().info(f'{self.name} -- Node {name} of type {node_type} is not an activation source')
+        else:
+            self.get_logger().error(f'{self.name} -- Tried to add {name} to activation inputs more than once')
+    
+    def delete_activation_input(self, node: dict): #Deletes a node from the activation inputs list. By default reads activations.
+        name=node['name']
+        if name in self.activation_inputs:
+            self.destroy_subscription(self.activation_inputs[node]['subscription'])
+            self.activation_inputs.pop(name)
+    
+    def configure_activation_inputs(self, neighbor_list):
+        for neighbor in neighbor_list:
+            self.create_activation_input(neighbor)
+
+    def read_activation_callback(self, msg: Activation):
+        node_name=msg.node_name
+        if node_name in self.activation_inputs:
+            if Time.from_msg(msg.timestamp).nanoseconds>Time.from_msg(self.activation_inputs[node_name]['data'].timestamp).nanoseconds:
+                self.activation_inputs[node_name]['data']=msg
+                self.activation_inputs[node_name]['updated']=True
+            elif Time.from_msg(msg.timestamp).nanoseconds<Time.from_msg(self.activation_inputs[node_name]['data'].timestamp).nanoseconds:
+                self.get_logger().warn(f'{self.name} -- Detected jump back in time, activation of node: {node_name} ({msg.node_type})')
         
     def __str__(self):
         """
