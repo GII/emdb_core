e0333f2cd377452f2d4f19b76319f96202f68217
Sergio Martínez Alonso <sergio.martinez3@udc.es>
2024-08-02 13:37:47 +0200

    Node_activations file added

--- core/core/file.py
+++ core/core/file.py
@@ -1,3 +1,9 @@
+from rclpy.time import Time
+
+from cognitive_node_interfaces.msg import Activation
+from cognitive_processes_interfaces.msg import Episode
+from core.utils import perception_msg_to_dict
+
 class File():
     """A MDB file."""
 
@@ -36,7 +42,7 @@ class FileGoodness(File):
 
     def write(self):
         """Write statistics data."""
-        formatted_goals = {goal: f"{reward:.1f}" for goal, reward in self.node.stm.reward.items()}
+        formatted_goals = {goal: f"{reward:.1f}" for goal, reward in self.node.stm.reward_list.items()}
 
         self.file_object.write(
             str(self.node.iteration)
@@ -76,3 +82,91 @@ class FilePNodesSuccess(File):
                     + "\n"
                 )
             self.node.pnodes_success[pnode] = None
+
+
+class FileNodeActivations(File):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.node_subscriptions = []
+        self.write_header()
+        self.check_subscriptions()
+
+    def write_header(self):
+        """Write the header of the file."""
+        super().write_header()
+        self.file_object.write(
+            "Timestamp\tNode name\tNode type\tActivation\n"
+        )
+
+    def check_subscriptions(self):
+        for node in self.node.LTM_cache:
+            if node['name'] not in self.node_subscriptions:
+                subscriber = self.node.create_subscription(
+                Activation,
+                'cognitive_node/' + str(node['name']) + '/activation',
+                self.receive_activation_callback,
+                1,
+                callback_group=self.node.cbgroup_loop
+                )
+                self.node_subscriptions.append(node['name'])
+
+    def receive_activation_callback(self, msg: Activation):
+        self.file_object.write(
+            str(Time.from_msg(msg.timestamp).nanoseconds)
+            + "\t"
+            + msg.node_name
+            + "\t"
+            + msg.node_type
+            + "\t"
+            + str(msg.activation)
+            + "\n"
+        )
+
+    def write(self):
+        self.check_subscriptions()
+
+# WORK IN PROGRESS
+
+# class FilesEpisodes(File):
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+#         self.write_header()
+#         self.node.create_subscription(
+#             Episode,
+#             'main_loop/episodes',
+#             self.receive_episodes_callback,
+#             1,
+#             callback_group = self.node.cbgroup_loop
+#         )
+    
+#     def write_header(self):
+#         """Write the header of the file."""
+#         super().write_header()
+#         self.file_object.write(
+#             "Timestamp\tOld perception\tOld LTM state\tPolicy\tPerception\tLTM state\tReward list\n"
+#         )
+    
+#     def receive_episodes_callback(self, msg: Episode):
+#         self.file_object.write(
+#             str(Time.from_msg(msg.timestamp).nanoseconds)
+#             + "\t"
+#             + str(perception_msg_to_dict(msg.old_perception))
+#             #+ "\t"
+#             #+ msg.old_ltm_state
+#             + "\t"
+#             + msg.policy
+#             + "\t"
+#             + str(perception_msg_to_dict(msg.perception))
+#             #+ "\t"
+#             #+ msg.ltm_state
+#             + "\t"
+#             + msg.reward_list
+#             + "\n"
+#         )
+
+#     def write(self):
+#         pass
+
+
+
